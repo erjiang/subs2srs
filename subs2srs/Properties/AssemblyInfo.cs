fcdb456c826097ae309f03d834f0efe58723fb9f
Eric Jiang <eric@fabrica.dev>
2020-06-25 21:42:54 -0700

    Bump version to 29.8.0.1

--- subs2srs/Properties/AssemblyInfo.cs
+++ subs2srs/Properties/AssemblyInfo.cs
@@ -25,12 +25,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("29.6.0.0")]
-[assembly: AssemblyFileVersion("29.6.0.0")]
+[assembly: AssemblyVersion("29.8.0.1")]
+[assembly: AssemblyFileVersion("29.8.0.1")]
